58fcfeb2c6bf5467dbf91d83fd962c170cc3f9dc
James Gregory <james@jagregory.com>
2013-08-28 14:22:27 +1000

    Export Root

--- root.go
+++ root.go
@@ -1,11 +1,11 @@
 package hypermedia
 
-type root struct {
+type Root struct {
 	Links HyperlinkSet `json:"links"`
 }
 
 // Create a root resource representation, which contains
 // a series of hyperlinks to other resources with no content.
-func Root(links ...Hyperlink) root {
-	return root{Links(links...)}
+func NewRoot(links ...Hyperlink) Root {
+	return Root{Links(links...)}
 }
