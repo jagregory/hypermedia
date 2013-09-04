36bb02e8f86227a9083b858520433fcc729f9d64
James Gregory <james@jagregory.com>
2013-09-05 06:46:52 +1000

    Can set a modtime on the root resource

--- root.go
+++ root.go
@@ -1,11 +1,25 @@
 package hypermedia
 
+import "time"
+
 type Root struct {
-	Links HyperlinkSet `json:"links"`
+	Links   HyperlinkSet `json:"links"`
+	modtime *time.Time
+}
+
+func (r Root) Modtime() *time.Time {
+	return r.modtime
 }
 
 // Create a root resource representation, which contains
 // a series of hyperlinks to other resources with no content.
 func NewRoot(links ...Hyperlink) Root {
-	return Root{Links(links...)}
+	return Root{Links(links...), nil}
+}
+
+// Create a root resource representation, which contains
+// a series of hyperlinks to other resources with no content
+// and a Last-Modified time.
+func NewMRoot(modtime time.Time, links ...Hyperlink) Root {
+	return Root{Links(links...), &modtime}
 }
