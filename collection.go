8a5f075c68877814a92a2464a9fbbb7b7edfa419
James Gregory <james@jagregory.com>
2013-09-15 16:46:17 +1000

    Breaking change. Remove resource for easier serialisation of collections

--- collection.go
+++ collection.go
@@ -1,33 +1,15 @@
 package hypermedia
 
-import "time"
-
 // Collection of Resources, with an hyperlink collection.
 type Collection struct {
 	Links      HyperlinkSet `json:"links"`
-	Collection []Resource   `json:"collection"`
-	modtime    *time.Time
-}
-
-func (c Collection) Modtime() *time.Time {
-	return c.modtime
+	Collection interface{}  `json:"collection"`
 }
 
 // Create a new collection of Resources, with a set of links.
-func NewCollection(resources []Resource, links ...Hyperlink) Collection {
-	var modtime *time.Time = nil
-
-	for _, res := range resources {
-		imod := res.Modtime()
-
-		if imod != nil && (modtime == nil || modtime.Before(*imod)) {
-			modtime = imod
-		}
-	}
-
+func NewCollection(collection interface{}, links ...Hyperlink) Collection {
 	return Collection{
 		Links:      Links(links...),
-		Collection: resources,
-		modtime:    modtime,
+		Collection: collection,
 	}
 }
